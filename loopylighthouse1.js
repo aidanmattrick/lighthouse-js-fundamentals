ff3bb3d0546c1aeb4a64bcbd62f57454a1b4b4f6
Aidan <aidan.mattrick@gmail.com>
2019-05-27 20:29:17 +0000

    looping exercise with multiple division conditionals

---
+++ loopylighthouse1.js
@@ -0,0 +1,18 @@
+for (var i = 100; i < 201; i++) {
+  if (i % 3 === 0 && i % 4 !== 0){
+    console.log("Loopy");
+  }
+  else{
+    if (i % 4 === 0 && i % 3 !== 0){
+      console.log("Lighthouse");
+    }
+    else{
+      if(i % 4 === 0 && i % 3 === 0){
+        console.log("LoopyLighthouse");
+      }
+      else{
+        console.log(i);
+      }
+    }
+  }
+}
