fb1ff16946ef02c45c54e13976f420dd8143ef7d
Aidan <aidan.mattrick@gmail.com>
2019-05-27 00:29:17 +0000

    whichSchool

---
+++ whichSchool.js
@@ -0,0 +1,12 @@
+function whichSchool(age) {
+  if (age < 13){
+    return "Elementary School";
+  }
+  else if (age > 12 && age < 19){
+    return "Secondary School";
+  }
+  else{
+    return "Lighthouse Labs";
+  }
+}
+console.log(whichSchool(7));
